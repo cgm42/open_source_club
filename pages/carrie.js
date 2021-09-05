b7d008cb5b08dd57c4f3c00bd8db604eedff78eb
Carrie <carr1eg@hotmail.com>
2021-09-04 18:29:05 -0700

    Add Carrie's page with weather

---
+++ pages/carrie.js
@@ -0,0 +1,32 @@
+import React from "react";
+import { useState, useEffect } from "react";
+import axios from "axios";
+const TEST_URL = "https://api.openweathermap.org/data/2.5/weather?q=Vancouver&units=metric&appid=";
+const api_key = process.env.NEXT_PUBLIC_OPEN_WEATHER_API_KEY;
+export default function Sardorbek() {
+	const [temp, setTemp] = useState(null);
+  const [date, setDate] = useState(null);
+
+  useEffect(() => {
+    axios
+      .get(TEST_URL + api_key)
+      .then(response => {
+        console.log(response);
+        setTemp(response.data.main.temp)
+        setDate(response.data.dt)
+      })
+  }, [])
+
+	return (
+    <div>
+			<div>Carrie</div>
+			<div>Today</div>
+			<div>{new Date(date * 1000).toString()}</div>
+			<div>{temp}</div>
+			<div>Carrie</div>
+			<div>Carrie</div>
+			<div>Carrie</div>
+			<div>Vancouver</div>
+		</div>
+	);
+}
--- pages/index.js
+++ pages/index.js
@@ -8,11 +8,6 @@ import Link from 'next/link';
 const TEST_URL = 'https://api.openweathermap.org/data/2.5/weather?q=London&appid=';
 
 export default function Home() {
-    useEffect(() => {
-        axios.get(TEST_URL + process.env.NEXT_PUBLIC_OPEN_WEATHER_API_KEY).then((data) => {
-            console.log(data);
-        });
-    }, []);
 
     return (
         <div className={styles.container}>
@@ -45,8 +40,14 @@ export default function Home() {
                         </Link>
                         <p>Las Vegas</p>
                     </div>
-                </div>
-            </main>
+					<div className={styles.card}>
+						<Link href="/carrie"passHref>
+							<h2>carrie</h2>
+						</Link>
+						<p>Vancouver</p>
+					</div>
+				</div>
+			</main>
 
             <footer className={styles.footer}>
                 <a
